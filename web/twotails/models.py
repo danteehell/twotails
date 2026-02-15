ea0e2c780bb45da5d09d8b1ef28d10c8a8f2a991
Darya15-4 <yasdarya7@gmail.com>
2026-02-15 19:56:54 +0300

    models added

--- web/twotails/models.py
+++ web/twotails/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.SET_DEFAULT, default=2)
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address_line = models.CharField(max_length=120)
+    city = models.CharField(max_length=20)
+    is_main = models.BooleanField(default=False)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+
+class Delivery(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('received', 'Received'),
+        ('canceled', 'Canceled'),
+    ]
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default='pending')
+    delivery_date = models.DateTimeField(auto_now_add=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    purchase_price = models.IntegerField()
+    sale_price = models.IntegerField()
+    manufacturer = models.CharField(max_length=100)
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    promotions = models.ManyToManyField('Promotion', through='ProductPromotion')
+
+class DeliveryItem(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.PositiveIntegerField()
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('converted', 'Converted'),
+        ('abandoned', 'Abandoned')
+    ]
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ('created', 'Created'),
+        ('paid', 'Paid'),
+        ('shipped', 'Shipped'),
+        ('cancelled', 'Cancelled'),
+    ]
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES)
+    total_amount = models.FloatField()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    price_at_purchase = models.FloatField()
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    discount_percent = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField()
+
+class ProductPromotion(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
